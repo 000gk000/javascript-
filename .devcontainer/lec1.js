f211e6538aeda1668d26e71f063c38c3a8cbdbea
Gopal <86468278+000gk000@users.noreply.github.com>
2023-07-04 07:19:30 +0000

    lec 1 part 2

--- .devcontainer/lec1.js
+++ .devcontainer/lec1.js
@@ -1,1 +1,16 @@
 console.log("hi")
+const phonenumber = 942892
+const phonen2=203830
+let name = "gopal"
+let name2 = "shashi"
+let name3 // here we just created and remain it left then it shows undefined value means it is not defined right noe 
+console.table([phonenumber,name,phonen2,name2,name3]) // if you have many constant and variable then we have to write console.log many times 
+// so , simply use console.table and 
+
+
+//-----------------------------NOTE-------------------------------
+/*
+        1.use only const and let in javascript for constant and variable respectively 
+        2.var is not used generally because of its no scope means var variable may be changed and get reclected back to everywhere 
+        
+*/
